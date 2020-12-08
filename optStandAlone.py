02bf24ba088e31050c95076281c38e99a3b39b88
victor <vctgeraldo@gmail.com>
2020-12-07 22:05:48 -0300

    Cod version 1

--- optStandAlone.py
+++ optStandAlone.py
@@ -20,6 +20,8 @@ from pyomo.environ import *
 import numpy as np
 import matplotlib.pyplot as plt
 
+#---Library to handle dataframes
+import pandas as pd
 # ------------------------------------------------------------#
 
 
@@ -31,18 +33,18 @@ import matplotlib.pyplot as plt
 
 
 #---Irr
-def irrResult(CA,EC,EU):
+def irrResult(CA,ECF,EU):
 
-    return 1.987347 + 0.652139*CA - 0.215191*CA*CA - 0.021653*EC - 0.047731*EC*EC \
-           - 0.000052*EU -0.054590*EU*EU - 0.015525*CA*EC - 0.035575*CA*EU \
-           + 0.008675*EC*EU
+    return 1.987347 + 0.652139*CA - 0.215191*CA*CA - 0.021653*ECF - 0.047731*ECF*ECF \
+           - 0.000052*EU -0.054590*EU*EU - 0.015525*CA*ECF - 0.035575*CA*EU \
+           + 0.008675*ECF*EU
 
 #---Avoided CO2
-def co2AvoidedResult(CA,EC,EU):
+def co2AvoidedResult(CA,ECF,EU):
 
-    return 26.64475 + 1.68662*CA - 0.50258*CA*CA + 0.28547*EC + 0.02429*EC*EC\
-           - 2.54509*EU + 0.53556*EU*EU + 0.02525*CA*EC + 0.37715*CA*EU \
-           - 0.04118*EC*EU
+    return 26.64475 + 1.68662*CA - 0.50258*CA*CA + 0.28547*ECF + 0.02429*ECF*ECF\
+           - 2.54509*EU + 0.53556*EU*EU + 0.02525*CA*ECF + 0.37715*CA*EU \
+           - 0.04118*ECF*EU
 #------------------------------------------------------------#
 
 
@@ -54,19 +56,19 @@ def co2AvoidedResult(CA,EC,EU):
 #---IRR
 def irr(modelo,flag,Irr_sup):
 
-    return (1.987347 + 0.652139 * modelOpt.CA - 0.215191 * modelOpt.CA * modelOpt.CA - 0.021653 * modelOpt.EC \
-            - 0.047731 * modelOpt.EC * modelOpt.EC - 0.000052 * modelOpt.EU - 0.054590 * modelOpt.EU * modelOpt.EU \
-            - 0.015525 * modelOpt.CA * modelOpt.EC - 0.035575 * modelOpt.CA * modelOpt.EU + \
-            0.008675 * modelOpt.EC * modelOpt.EU) / Irr_sup
+    return (1.987347 + 0.652139 * modelOpt.CA - 0.215191 * modelOpt.CA * modelOpt.CA - 0.021653 * modelOpt.ECF \
+            - 0.047731 * modelOpt.ECF * modelOpt.ECF - 0.000052 * modelOpt.EU - 0.054590 * modelOpt.EU * modelOpt.EU \
+            - 0.015525 * modelOpt.CA * modelOpt.ECF - 0.035575 * modelOpt.CA * modelOpt.EU + \
+            0.008675 * modelOpt.ECF * modelOpt.EU) / Irr_sup
 
 
 #---CO2 avoided
 def co2_avoid(modelo,flag,COdois_sup):
 
-    return (26.64475 + 1.68662 * modelOpt.CA - 0.50258 * modelOpt.CA * modelOpt.CA + 0.28547 * modelOpt.EC
-            + 0.02429 * modelOpt.EC * modelOpt.EC - 2.54509 * modelOpt.EU + 0.53556 * modelOpt.EU * modelOpt.EU
-            + 0.02525 * modelOpt.CA * modelOpt.EC + 0.37715 * modelOpt.CA * modelOpt.EU
-            - 0.04118 * modelOpt.EC * modelOpt.EU) / COdois_sup
+    return (26.64475 + 1.68662 * modelOpt.CA - 0.50258 * modelOpt.CA * modelOpt.CA + 0.28547 * modelOpt.ECF
+            + 0.02429 * modelOpt.ECF * modelOpt.ECF - 2.54509 * modelOpt.EU + 0.53556 * modelOpt.EU * modelOpt.EU
+            + 0.02525 * modelOpt.CA * modelOpt.ECF + 0.37715 * modelOpt.CA * modelOpt.EU
+            - 0.04118 * modelOpt.ECF * modelOpt.EU) / COdois_sup
 
 #------------------------------------------------------------#
 
@@ -90,8 +92,9 @@ def mainModel(w, flag, Irr_sup,COdois_sup):
 
     #---Instantiating the variables of the model, and their
     #respective baundaries
+    
     modelOpt.CA = Var(bounds=(-1.681792831, 1.681792831))
-    modelOpt.EC = Var(bounds=(-1.681792831, 1.681792831))
+    modelOpt.ECF = Var(bounds=(-1.681792831, 1.681792831))
     modelOpt.EU = Var(bounds=(-1.681792831, 1.681792831))
 
     #Defining the objective function
@@ -110,7 +113,7 @@ def mainModel(w, flag, Irr_sup,COdois_sup):
     opt = SolverFactory('ipopt')
     resultado = opt.solve(modelOpt)
 
-    return modelOpt.CA.value, modelOpt.EC.value, modelOpt.EU.value
+    return modelOpt.CA.value, modelOpt.ECF.value, modelOpt.EU.value
 #------------------------------------------------------------#
 
 
@@ -121,65 +124,73 @@ def mainModel(w, flag, Irr_sup,COdois_sup):
 if __name__ == "__main__":
 
     #----Superior limit of Irr-----------#
-    CA, EC, EU = mainModel(1, 1, 1,1)
-    Irr_sup = irrResult(CA,EC,EU)
+    CA, ECF, EU = mainModel(1, 1, 1,1)
+    Irr_sup = irrResult(CA,ECF,EU)
     print(Irr_sup)
 
     # ----Superior limit of CO2-----------#
-    CA, EC, EU = mainModel(1, 2, 1,1)
-    COdois_sup = co2AvoidedResult(CA, EC, EU)
+    CA, ECF, EU = mainModel(1, 2, 1,1)
+    COdois_sup = co2AvoidedResult(CA, ECF, EU)
     print(COdois_sup)
 
 
-    #Valores nos pesos variando entre 0 e 1
-    # na variavel num temos numero de elementos que estarão presentes
+    #weight of the objective function varying between 0 and 1
     val_w = np.linspace(0.0, 1.0, num=10000)
 
-    #Lista de variaveis
+
+    #------------------------------------------------------------#
+    #             DEFINING THE LIST OF REPORTED RESULTS
+    #------------------------------------------------------------#
+
+
+    #List of variable
     CA_lista = []
-    EC_lista = []
+    ECF_lista = []
     EU_lista = []
 
-    #Lista de pesos
+    #List of weight
     w_irr = []
     w_co2 = []
 
-    #Lista contendo os valores das obj funtions
+    #List of the result
     irr_lista = []
     co2_lista = []
 
 
     i = 0
 
+    #---Iterating over the pareto points	
     for w in val_w:
 
-        #Rodando o modelo de otimizacao
-        CA, EC, EU = mainModel(w, 3,Irr_sup,COdois_sup)
+        #Running the model
+        CA, ECF, EU = mainModel(w, 3,Irr_sup,COdois_sup)
 
-        #Quardando as variaveis codificadas
+	#--Storing the results of the optimization 
+	
+        #---Coded variables 
         CA_lista.append( CA )
-        EC_lista.append(EC)
+        ECF_lista.append(ECF)
         EU_lista.append(EU)
 
-        #Guardando os valores da funcao objetivo
-        irr_lista.append( irrResult(CA,EC,EU) )
-        co2_lista.append( co2AvoidedResult(CA, EC, EU) )
+        #Objective function
+        irr_lista.append( irrResult(CA,ECF,EU) )
+        co2_lista.append( co2AvoidedResult(CA, ECF, EU) )
 
         #Guardando os pesos
         w_irr.append(w)
         w_co2.append(1.0-w)
 
-        #Contador para mostrar a evolucao do processamento
+        
         i += 1
         print(i)
 
 
 
-    #Guardando respostas
+    #Constructing a dictionary with the results
     dt = {
 
         'CA' : CA_lista,
-        'EC' : EC_lista,
+        'ECF' : ECF_lista,
         'EU' : EU_lista,
         'irr_obj': irr_lista,
         'CO2_obj': co2_lista,
@@ -188,15 +199,16 @@ if __name__ == "__main__":
 
     }
 
-    #Salvando em um csv
+    #Saving results in a pandas dataframes and write it down on a 
+    #".csv" file.
     pd.DataFrame(dt).to_csv('2G_standAlone.csv', sep = ';', index=False)
 
 
 
-    #Gerando o grafico com os resultados
+    #Generating graph with the pareto frontier
     plt.scatter(x = irr_lista, y= co2_lista)
-    plt.title("Fronteira de Pareto para 10000 pontos")
-    plt.ylabel("CO2 evitado")
+    plt.title("Pareto frontier")
+    plt.ylabel("CO2 evoided")
     plt.xlabel("IRR")
     plt.show()
 #------------------------------------------------------------#
