845de56743f297f505b5a207b78db6d5c9b5f941
victor <vctgeraldo@gmail.com>
2021-01-04 21:37:03 -0300

    1G2G first version in Eng

--- opt_1G2G.py
+++ opt_1G2G.py
@@ -1,60 +1,111 @@
-# Importando biblioteca usadas
+#------------------------------------------------------------#
+#Title: Optimization of a 1G 2G mill...                      #
+#Authors:                                                    #
+#    1 - Jessica Marcon Bressanin                            #
+#    2 - Victor Coelho Geraldo                               #
+#      - ...                                                 #
+#                                                            #
+#Institution: CTBE                                           #
+#------------------------------------------------------------#
+
+
+#------------------------------------------------------------#
+#                   IMPORTING LIBRARIES                      #
+#------------------------------------------------------------#
+
+#---Pyomo for optimization
 from pyomo.environ import *
+
+#---Numerical and plot libraries
 import numpy as np
 import matplotlib.pyplot as plt
+
+#---Library to handle dataframes
 import pandas as pd
+# ------------------------------------------------------------#
 
 
+#------------------------------------------------------------#
+#     COMPUTING THE RESULTS OF THE OPTMIZATION PROCEDURE     #
+#------------------------------------------------------------#
+#The following functions (i.e., irrResult, co2AvoidedResult)
+#compute the results of the model.
 
+
+#---Irr
 def irr_resultado(CA,EC,EU):
     return 8.903801 + 0.820775 * CA - 0.809301 * CA *CA - 0.637673 *EC \
            - 0.087893 * EC * EC - 0.233889 * EU - 0.387459 *EU * EU \
            - 0.213700 * CA * EC + 0.600275 *CA * EU + 0.339975 * EC * EU
-
+#---Avoided CO2
 def co2_avoid_resultado(CA,EC,EU):
     return 18.90206 + 0.83915 * CA - 0.37436 * CA * CA + 0.74775 *EC \
            + 0.03957 * EC * EC - 1.00481 * EU + 0.01141 * EU * EU \
            + 0.05126 * CA * EC + 0.42576 * CA * EU - 0.04161 * EC *EU
+#------------------------------------------------------------#
 
 
-#Restricao
-def restricao(CA,EC,EU):
-    return 2.95291-3.29602*CA+1.75062*CA*CA -1.18355*EC+0.03588*EC*EC+3.37391*EU\
-           +0.51318*EU*EU + 0.16250*CA*EC  -2.18750*CA*EU  -0.61250*EC*EU
 
+#------------------------------------------------------------#
+#                    OBJETIVE FUNCTION                       #
+#------------------------------------------------------------#
 
-#FUNCAO PARA USAR NOS MODELOS
+#---IRR
 def irr(modelo,flag,Irr_sup):
     return (8.903801 + 0.820775 * modelo.CA - 0.809301 * modelo.CA * modelo.CA - 0.637673 * modelo.EC \
            - 0.087893 * modelo.EC * modelo.EC - 0.233889 * modelo.EU - 0.387459 * modelo.EU * modelo.EU \
            - 0.213700 * modelo.CA * modelo.EC + 0.600275 * modelo.CA * modelo.EU + 0.339975 * modelo.EC \
            * modelo.EU)/Irr_sup
 
+#---CO2 avoided
 def co2_avoid(modelo,flag,COdois_sup):
     return (18.90206 + 0.83915 * modelo.CA - 0.37436 * modelo.CA * modelo.CA + 0.74775 * modelo.EC \
            + 0.03957 * modelo.EC * modelo.EC - 1.00481 * modelo.EU + 0.01141 * modelo.EU * modelo.EU \
            + 0.05126 * modelo.CA * modelo.EC + 0.42576 * modelo.CA * modelo.EU - 0.04161 * modelo.EC \
           * modelo.EU)/COdois_sup
 
+#------------------------------------------------------------#
+#                    RESTRICTION FUNCTION                    #
+#------------------------------------------------------------#
+
+def restricao(CA,EC,EU):
+    return 2.95291-3.29602*CA+1.75062*CA*CA -1.18355*EC+0.03588*EC*EC+3.37391*EU\
+           +0.51318*EU*EU + 0.16250*CA*EC  -2.18750*CA*EU  -0.61250*EC*EU
 #-----------------------------------------------------------------
+
+
+
+#------------------------------------------------------------#
+#                       MAIN MODEL
+#------------------------------------------------------------#
+
+#In the function bellow (i.e., mainModel), it is defined the
+#optmization model. Therefore, it contains the equations repre-
+#senting the objective function and the restrictions.
+#"mainModel" also contains the calling of Pyomo library, as well
+#as the definition of the solver used in the optimization proce-
+#dure.
+
 def rodando_modelo(w, flag, Irr_sup,COdois_sup):
 
-    # Instanciando o modelo que usaremos
+    #---Defining the optimization model
     modelo = ConcreteModel(name="(Jessica_doc)")
+    
 
-    # Definindo variaveis do modelo
+    #---Instantiating the variables of the model, and their
+    #respective baundaries
     modelo.CA = Var(bounds=(-1.681792831, 1.681792831))
     modelo.EC = Var(bounds=(-1.681792831, 1.681792831))
     modelo.EU = Var(bounds=(-1.681792831, 1.681792831))
 
-    #Restricoes
+    #Defining the restriction function. 
     modelo.rest = Constraint(expr=2.95291-3.29602*modelo.CA+1.75062*modelo.CA*modelo.CA
                          -1.18355*modelo.EC+0.03588*modelo.EC*modelo.EC+3.37391*modelo.EU
                          +0.51318*modelo.EU*modelo.EU+0.16250*modelo.CA*modelo.EC
                          -2.18750*modelo.CA*modelo.EU-0.61250*modelo.EC*modelo.EU >= 0.00001)
 
 
-    # Definindo funcao objetivo
+    #Defining the objective function
 
     if flag ==1:
         modelo.obj = Objective(expr=irr(modelo,flag,Irr_sup), sense=-1)
@@ -66,7 +117,7 @@ def rodando_modelo(w, flag, Irr_sup,COdois_sup):
         modelo.obj = Objective(expr=w * irr(modelo,flag,Irr_sup) \
                      + (1.0 - w) * co2_avoid(modelo,flag,COdois_sup), sense=-1)
 
-    #Resolvendo o problema de otimização
+    #Solving the optmization model
     opt = SolverFactory('ipopt')
     resultado = opt.solve(modelo)
 
@@ -74,33 +125,41 @@ def rodando_modelo(w, flag, Irr_sup,COdois_sup):
 #----------------------------------------------------------------------------
 
 
+#------------------------------------------------------------#
+#                  RUNNING THE OPTMIZATION MODEL
+#------------------------------------------------------------#
+
 if __name__ == "__main__":
 
-    #----Limite sup Irr-----------#
+    #----Superior limit of Irr-----------#
     CA, EC, EU = rodando_modelo(1, 1, 1,1)
     Irr_sup = irr_resultado(CA,EC,EU)
     print(Irr_sup)
 
-    # ----Limite sup CO2-----------#
+    # ----Superior limit of CO2-----------#
     CA, EC, EU = rodando_modelo(1, 2, 1,1)
     COdois_sup = co2_avoid_resultado(CA, EC, EU)
     print(COdois_sup)
 
 
-    #Valores nos pesos variando entre 0 e 1
-    # na variavel num temos numero de elementos que estarão presentes
+    #weight of the objective function varying between 0 and 1
     val_w = np.linspace(0.0, 1.0, num=10000)
 
-    #Lista de variaveis
+    #------------------------------------------------------------#
+    #             DEFINING THE LIST OF REPORTED RESULTS
+    #------------------------------------------------------------#
+
+
+     #List of variable
     CA_lista = []
     EC_lista = []
     EU_lista = []
 
-    #Lista de pesos
+    #List of weight
     w_irr = []
     w_co2 = []
 
-    #Lista contendo os valores das obj funtions
+    #List of the result
     irr_lista = []
     co2_lista = []
 
@@ -109,34 +168,37 @@ if __name__ == "__main__":
 
     i = 0
 
+    #---Iterating over the pareto points
     for w in val_w:
 
-        #Rodando o modelo de otimizacao
+        #Running the model
         CA, EC, EU = rodando_modelo(w, 3,Irr_sup,COdois_sup)
 
-        #Quardando as variaveis codificadas
+        #--Storing the results of the optimization 
+	
+        #---Coded variables 
         CA_lista.append( CA )
         EC_lista.append(EC)
         EU_lista.append(EU)
 
-        #Guardando os valores da funcao objetivo
+        #Objective function
         irr_lista.append( irr_resultado(CA,EC,EU) )
         co2_lista.append( co2_avoid_resultado(CA, EC, EU) )
 
-        #Guardando os pesos
+        #weight values
         w_irr.append(w)
         w_co2.append(1.0-w)
 
-        #Guardando restricao
+        #Restriction values
         restricao_lista.append(restricao(CA, EC, EU))
 
-        #Contador para mostrar a evolucao do processamento
+        
         i += 1
         print(i)
 
 
 
-#Guardando respostas
+#Constructing a dictionary with the results
 dt = {
 
     'CA' : CA_lista,
@@ -150,19 +212,18 @@ dt = {
 
 }
 
-#Salvando em um csv
+#Saving results in a pandas dataframes and write it down on a 
+#".csv" file.
 pd.DataFrame(dt).to_csv('resultados.csv', sep = ';', index=False)
 
 
-
-
-
-
-
-
-#Gerando o grafico com os resultados
+#Generating graph with the pareto frontier
 plt.scatter(x = irr_lista, y= co2_lista)
 plt.title("Fronteira de Pareto para 10000 pontos")
 plt.ylabel("CO2 evitado")
 plt.xlabel("IRR")
 plt.show()
+
+
+
+
